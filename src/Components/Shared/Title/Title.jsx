04f7b097f8bc25f087d322e6bfff638bfb5bb13e
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-28 23:34:10 +0600

    shared title done

--- src/Components/Shared/Title/Title.jsx
+++ src/Components/Shared/Title/Title.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function Title({heading, subHeading}) {
-    return (
-        <div>
-            
-        </div>
-    )
+function Title({ heading, subHeading }) {
+  return (
+    <div className="md:w-3/12 mx-auto my-8 text-center space-y-3">
+      <h3 className="text-yellow-500 text-md italic">--- {subHeading} ---</h3>
+      <h1 className="border-y-4 py-5 text-2xl font-semibold">{heading}</h1>
+    </div>
+  );
 }
 
-export default Title
+export default Title;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Bannar from './Bannar/Bannar';
+import Title from '../../Components/Shared/Title/Title';
 
 function Home() {
     return (
         <div>
            <Bannar></Bannar>
+           <Title 
+            subHeading={'From 11:00am to 10:00pm'}
+            heading={'ORDER ONLINE'}
+           ></Title>
         </div>
     )
 }
