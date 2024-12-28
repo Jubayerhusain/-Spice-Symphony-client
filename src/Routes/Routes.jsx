832c69f199cbbf29e2eb2242189f222d8e2a4fb6
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-28 22:28:56 +0600

    Navbar set up done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,18 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout/MainLayout";
+import Home from "../Pages/Home/Home";
   
 export const Routes = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+            path:'/',
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
