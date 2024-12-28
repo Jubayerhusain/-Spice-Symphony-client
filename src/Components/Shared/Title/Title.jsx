c4981d53fec1623beaf44f83303ee15aaa73c392
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-28 23:26:44 +0600

    Carousel done

---
+++ src/Components/Shared/Title/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title({heading, subHeading}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Title
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Bannar from './Bannar/Bannar';
 
 function Home() {
     return (
         <div>
-            this is a home component
+           <Bannar></Bannar>
         </div>
     )
 }
